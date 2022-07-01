460632e0ca4896039bdd91da06c78fa9004b4440
Afolami John <afolami.john@gmail.com>
2022-07-01 11:47:25 -0700

    Pushing task 1 to 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase,
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+char al;
+for (al = 'a' && al = 'A'; al <= 'a' && al <='A'; al++)
+{
+putchar(al);
+}
+putchar('\n');
+return (0);
