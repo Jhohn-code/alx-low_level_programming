22aeec73b64c08ab0bfffd90e01e291c5eab2c11
Afolami John <afolami.john@gmail.com>
2022-07-01 10:26:18 -0700

    Pushing the folder and task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -23,14 +23,15 @@ int main(void)
 	/* your code goes there */
 	if (n > 0)
 	{
-	printf("This number: %d is positive\n", n);
-	} else if (n == 0)
+	printf("%d is positive\n", n);
+	}
+	else if (n == 0)
 	{
-	printf("This number: %d is zero\n", n);
-	} 
-	else 
+	printf("%d is zero\n", n);
+	}
+	else
 	{
-	printf("This number: %d is negative\n", n);
+	printf("%d is negative\n", n);
 	}
 	return (0);
 }
