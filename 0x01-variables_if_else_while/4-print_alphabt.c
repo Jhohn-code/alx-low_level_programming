433fca4f1762c6beb34901db4a8600a783eb969f
Afolami John <afolami.john@gmail.com>
2022-07-01 12:19:28 -0700

    Pushing task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main  prints the alphabet in lowercase
+ * except e and q
+ * return: Always 0 success
+ */
+
+int main(void)
+char al = 'a';
+
+while (al <= 'z')
+{
+if (al != 'e' && al != 'q')
+{
+putchar(al);
+}
+al++
+}
+putchar(\n);
+return (0);
