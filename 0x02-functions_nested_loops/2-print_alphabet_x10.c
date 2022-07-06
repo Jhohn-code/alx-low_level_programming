336976a9327f164e2342d70bfd7ea948cf0293e5
Afolami John <afolami.john@gmail.com>
2022-07-06 13:26:49 -0700

    Pushing task 1 and 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * main - prints 10 times the alphabet, in lowercase, followed by a new line.
+ *
+ * Return: nothing
+ */
+
+void print_alphabet_x10(void)
+{
+	char c;
+	int i;
+
+	i = 0;
+
+	while(i < 10)
+	{
+		c = 'a';
+		while(c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+	_putchar('\n');
+	i++;
+	}
+}
