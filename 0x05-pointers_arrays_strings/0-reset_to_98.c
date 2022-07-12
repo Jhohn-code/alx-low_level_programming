713b335bf3b6aa6a2be122994f82534842b65017
Afolami John <afolami.john@gmail.com>
2022-07-12 16:44:39 -0700

    Pushing directory and task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *
+ *
+ */
+
+void reset_to_98(int *n)
+{
+	int v;
+	
+	*n = v;
+	v = 402;
+	_putchar ("%d\n", v);
+	n = 98;
+	_putchar ("%d\n", v)
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This is ALX task 0x05-pointers_arrays_strings
