b586316b997c1f9b8330103d6abaccc10a198bf7
Afolami John <afolami.john@gmail.com>
2022-07-12 17:33:46 -0700

    Pushing task 1 to 5

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * void swap_int - swap the value of two integer
+ * @a: pointer to the first value
+ * @b: pointer to the second value
+ */
+
+void swap_int(int *a, int *b)
+{
+	int c;
+
+	c = *a;
+	*a = *b;
+	*b = c;
+}
