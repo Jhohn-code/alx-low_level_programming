7714a1ca1632808c5f0e5983f792c5a1f60587f3
Afolami John <afolami.john@gmail.com>
2022-07-27 08:10:44 -0700

    Correecting task 2

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
 
 /**
- * main - prints the number of arguments passed into the program
- * @argc: int
- * @argv: list
+ * main - prints the number of arguments passed into it
+ *
+ * @argc: argument counter
+ * @argv: argument vector
+ *
  * Return: 0
  */
 
-int main(int argc, char const *argv[])
+int main(int argc, char *argv[])
 {
-(void)argv;
-printf("%i\n", argc - 1);
-return (0);
+	int c;
+
+	for (c = 0; c < argc; c++)
+		printf("%s\n", argv[c]);
+
+	return (0);
 }
