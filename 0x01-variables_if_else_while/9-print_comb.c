0c9f13b087309f74a1a354654f2999b44f0f3a72
root <root@Jhohn.localdomain>
2023-12-15 16:44:29 +0100

    added task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible combinations of single-digit numbers.
+ * Return: 0 if executed properly, non zero otherwise
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+		if (n != 57)
+		{
+		putchar(',');
+		putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return(0);
+}
