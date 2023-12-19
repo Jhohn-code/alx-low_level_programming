a4b92b6da59b29814f8da70d1252278250f51bc7
root <root@Jhohn.localdomain>
2023-12-19 20:09:07 +0100

    adding t6ask 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and return the result
+ * Result: 0 if executed properly, non zero otherwise
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
