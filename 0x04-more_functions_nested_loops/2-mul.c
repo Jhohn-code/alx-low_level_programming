a6a9b60487ffaa44297fb58698504024aa3083e2
root <root@Jhohn.localdomain>
2023-12-22 15:31:37 +0100

    corrected task 2

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,11 +1,11 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * mul - multiplies two integers
  * @a: the first character to be multiplied
  * @b: the second character
- * Return: 0 if executed properly, non zero otherwise */
+ * Return: 0 if executed properly, non zero otherwise
+ */
 
 int mul(int a, int b)
 {
