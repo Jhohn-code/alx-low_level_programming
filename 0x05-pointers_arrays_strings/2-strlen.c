7aa5a583814f45644ce301a897a09662cde89b62
root <root@Jhohn.localdomain>
2024-01-02 18:46:20 +0100

    added task 0 to 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string.
+ * @s: string to evaluate
+ * Return: return the length of the string
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != 0; i++)
+	{
+
+	}
+
+	return (i);
+}
