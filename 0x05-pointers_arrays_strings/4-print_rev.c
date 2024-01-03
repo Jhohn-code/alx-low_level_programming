312ecb464c3a750b29c95dd74b8fd0be927f2dc6
root <root@Jhohn.localdomain>
2024-01-03 15:38:10 +0100

    corrected task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,13 +11,12 @@ void print_rev(char *s)
 
 	for (i = 0; s[i] != 0; i++)
 	{
-	
 	}
 
-	for ( j = i; j > 0; j--)
+	for (j = i; j >= 0; j--)
 	{
 		_putchar(s[j]);
 	}
 
-	_putchar('\n');
+	/**_putchar('\n');*/
 }
